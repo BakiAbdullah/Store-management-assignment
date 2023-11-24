7ed4f96d90d6c089989948eca68f4ffd80e960e6
BakiAbdullah <shatil605@gmail.com>
2023-11-24 20:28:13 +0600

    initial project setup done

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import app from './app'
+import mongoose from 'mongoose'
+import config from './app/config'
+
+async function server() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('DB Connected')
+    app.listen(config.port, () => {
+      console.log(`Store app is listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+server().catch((err) => console.log(err))
