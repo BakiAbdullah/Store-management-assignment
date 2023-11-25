dcd830168a45eef844e0b2f678cde46c4fedfc62
BakiAbdullah <shatil605@gmail.com>
2023-11-25 10:33:56 +0600

    used express middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
-import express from "express"
-const app = express()
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+export const app: Application = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json())
+app.use(cors())
+// app.use('app/users', userRoute)
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Welcome to Store Management portal Server with Mongoose!',
+  })
 })
-
-export default app
-
